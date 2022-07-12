3a28011c0a8955973fd67e437321a25923618682
Hyacine Alnuma <hyacinealnuma@gmail.com>
2022-07-12 15:18:50 +0200

    Routing done 2

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -6,6 +6,10 @@ const auth = require('../middlewares/auth');
 const multer = require('../middlewares/multer-config');
 
 router.get('/', postsCtrl.getAllPosts);
+router.get('/:id', postsCtrl.getOnePost);
 router.post('/', postsCtrl.createPost);
+router.put('/:id', postsCtrl.updatePost);
+router.delete('/:id', postsCtrl.deletePost);
+router.post('/:id', postsCtrl.likePost);
 
 module.exports = router;
