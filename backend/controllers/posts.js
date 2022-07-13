3d0004fee789aec0b6fc8999b95bc021585cb503
Hyacine Alnuma <hyacinealnuma@gmail.com>
2022-07-13 13:40:41 +0200

    Controllers almost done

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,11 +1,11 @@
 const db = require('../db');
+const fs = require('fs');
 
 exports.getAllPosts = (req, res, next) => {
     let sql = 'SELECT * FROM posts';
-    let query = db.query(sql, (err, result) => {
+    let query = db.query(sql, (err, results) => {
         if (err) throw err;
-        console.log(result);
-        res.send(result);
+        res.status(200).json(results);
     });
 };
 
@@ -13,28 +13,79 @@ exports.getOnePost = (req, res, next) => {
     let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
     let query = db.query(sql, (err, result) => {
         if (err) throw err;
-        console.log(result);
-        res.send(result);
+        res.status(200).json(result);
     });
 };
 
 exports.createPost = (req, res, next) => {
     let postObject = req.body;
-    res.send(postObject);
-    // let sql = `INSERT INTO `posts` (`userId`, `textpost`, `imageUrl`, `likes`) VALUES (`${postObject.userId}`, `${postObject.textpost}`, `${postObject.imageUrl}`, `0`)`';
-    // let query = db.query(sql, (err, result) => {
-    //     if (err) throw err;
-    //     console.log(result);
-    //     res.send(result);
-    // })
-}
+    let sql = `INSERT INTO posts (userId, textpost, imageUrl, likes) VALUES ('${postObject.userId}', '${postObject.textpost}', '${postObject.imageUrl}', '0')`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.status(201).json({ message: 'Post enregistré !'})
+    });
+};
 
 exports.updatePost = (req, res, next) => {
-    let sql = 'UPDATE `posts` SET '
+    const postObject = req.file ?
+    {
+        ...req.body,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {...req.body};
+    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        if (!result) {
+            res.status(404).json({ error: new Error('Post inexistante !') });
+        }
+        // if (result[0].userId !== req.auth.userId) {
+        //     res.status(403).json({ error: new Error('Requête non authorisée !') });
+        // } 
+        if (!req.file) {
+            sql = `UPDATE posts SET textpost = '${postObject.textpost}' WHERE id = '${req.params.id}'`;
+            let query = db.query(sql, (err, result) => {
+                if (err) throw err;
+                res.status(201).json({ message: 'Post modifié !'})
+            })
+        } else {
+            const filename = result.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                sql = `UPDATE posts SET textpost = '${postObject.textpost}', imageUrl = '${postObject.imageUrl}' WHERE id = '${req.params.id}'`;
+                let query = db.query(sql, (err, result) => {
+                    if (err) throw err;
+                    res.status(201).json({ message: 'Post modifié !'})
+                });
+            });
+        }
+    });
 };
 
 exports.deletePost = (req, res, next) => {
-
+    let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err, result) => {
+        if (!result) {
+            res.status(404).json({ error: new Error('Sauce inexistante !') });
+        }
+        // if (result[0].userId !== req.auth.userId) {
+        //     res.status(403).json({ error: new Error('Requête non authorisée !') });
+        // } 
+        if (result[0].imageUrl !== null) {
+            const filename = result.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                sql = `DELETE FROM posts WHERE id = '${req.params.id}'`;
+                let query = db.query(sql, (err, result) => {
+                    if (err) throw err;
+                    res.status(201).json({ message: 'Post supprimé !'})
+                });
+            });
+        } else {
+            sql = `DELETE FROM posts WHERE id = '${req.params.id}'`;
+            let query = db.query(sql, (err, result) => {
+                if (err) throw err;
+                res.status(201).json({ message: 'Post supprimé !'})
+            });
+        }
+    });
 };
 
 exports.likePost = (req, res, next) => {
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,41 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const db = require('../db');
+
 exports.signup = (req, res, next) => {
- res.send("azy stp");
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        let sql = `INSERT INTO users (email, passwordhash) VALUES ('${req.body.email}', '${hash}')`;
+        let query = db.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+        });
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
-
+    let sql = `SELECT * FROM users WHERE email = '${req.body.email}'`;
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        if (!result) {
+            return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+        }
+        bcrypt.compare(req.body.password, result[0].passwordhash)
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({ error: 'Mot de passe incorrect !' });
+                }
+                res.status(200).json({
+                    userId: result[0].id,
+                    token: jwt.sign(
+                        { userId: result[0].id },
+                        'TOKEN',
+                        { expiresIn: '24h' }
+                    )
+                });
+            })
+    })
 };
