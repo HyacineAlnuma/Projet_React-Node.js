f2a8a197a727e37e63f37d77798e682903de9ea8
Hyacine Alnuma <hyacinealnuma@gmail.com>
2022-09-08 16:50:34 +0200

    Project done ? v2

--- frontend/src/pages/Profile/index.jsx
+++ frontend/src/pages/Profile/index.jsx
@@ -118,6 +118,7 @@ function Profile() {
         if (token == null) {
             navigate('/login');
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     function imageHandler(e) {
